1276b2e6763e761d25a8a796c0cb7191e259e681
Thomas Deranek <thomas.deranek@cerner.com>
2019-08-05 08:32:49 -0500

    Added Mobile and TurboMode Initial Skeleton

---
+++ lib/binger/bing_browser/desktop_bing_browser.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Binger
+  class DesktopBingBrowser
+    def send_search(options = {})
+      patiently_select(:text_field, name: 'q').yield_self do |search_bar|
+        search_bar.set(options[:custom_search] || randomized_query)
+      end
+
+      sleep(options[:pause_before]) if options[:pause_before]
+      patiently_select(:element, name: 'go').click
+      sleep(options[:pause_after]) if options[:pause_after]
+    end
+
+    private
+
+    def new_browser
+      Watir::Browser.new(:chrome)
+    end
+
+    def to_sign_in
+      patiently_select(:element, id: 'id_l').click
+    end
+  end
+end
---
+++ lib/binger/bing_browser/mobile_bing_browser.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Binger
+  class MobileBingBrowser < BingBrowser
+    def send_search(options = {})
+      patiently_select(:text_field, name: 'q').yield_self do |search_bar|
+        search_bar.set(options[:custom_search] || randomized_query)
+      end
+
+      sleep(options[:pause_before]) if options[:pause_before]
+      if browser.element(id: 'sbBtn').present?
+        patiently_select(:element, id: 'sbBtn').click
+      else
+        patiently_select(:element, id: 'sb_form_go').click
+      end
+      sleep(options[:pause_after]) if options[:pause_after]
+    end
+
+    private
+
+    def to_sign_in
+      patiently_select(:element, class: %w[closeIcon rms_img]).click
+      patiently_select(:element, id: 'mHamburger').click
+      patiently_select(:element, id: 'hb_s').click
+    end
+
+    def new_browser
+      driver = Webdriver::UserAgent.driver(browser: :chrome, agent: :iphone, orientation: :landscape)
+      Watir::Browser.new(driver)
+    end
+  end
+end
